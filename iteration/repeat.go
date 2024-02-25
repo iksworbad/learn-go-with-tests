678a65cd01086820fdb90e093f37ba3ac4cf68af
Kamil Dabrowski <kamillo.dabrowski@gmail.com>
2024-02-25 22:07:13 +0100

    refactor: repeat func

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
-func Repeat(text string) string {
+func Repeat(text string, repeatTime int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + text
+	for i := 0; i < repeatTime; i++ {
+		repeated += text
 	}
 	return repeated
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,13 +1,28 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 5)
+	expected := strings.Repeat("a", 5)
 
 	if repeated != expected {
 		t.Errorf("Expected %q but got %q", expected, repeated)
 	}
+}
 
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("foo", 2)
+	fmt.Println(repeat)
+	// Output: foofoo
 }
