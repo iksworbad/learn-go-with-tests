6f58ed5bd63d49a4651ec4fd615d7125057a2590
Kamil Dabrowski <kamillo.dabrowski@gmail.com>
2024-02-26 15:34:05 +0100

    feat: slice array

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -28,3 +28,33 @@ func TestSumAll(t *testing.T) {
 		}
 	})
 }
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(got []int, want []int, t *testing.T) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("test sum all tails with two arguments", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(got, want, t)
+	})
+
+	t.Run("test sum all tails with more arguments", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 1})
+		want := []int{5, 10}
+
+		checkSums(got, want, t)
+
+	})
+	t.Run("test sum all tails with empty array", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9, 1})
+		want := []int{0, 10}
+
+		checkSums(got, want, t)
+	})
+}
